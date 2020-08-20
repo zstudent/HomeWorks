c3abfe5ec35f2f8e43fc49aeda0b3b098cd5b0cd
NonaKuloshvili <nona.kuloshvili@gmail.com>
2020-08-21 00:05:36 +0400

    Added week 1 homework

---
+++ Algorithms for DNA Sequencing 1/assignment_1.py
@@ -0,0 +1,22 @@
+# Question 1
+# How many times does \verb|AGGT|AGGT or its reverse complement (\verb|ACCT|ACCT) occur in the lambda
+# virus genome? E.g. if \verb|AGGT|AGGT occurs 10 times and \verb|ACCT|ACCT occurs 12 times, you should report 22.
+
+
+def readGenome(filename):
+    genome = ''
+    with open(filename, 'r') as f:
+        for line in f:
+            # ignore header line with genome information
+            if not line[0] == '>':
+                genome += line.rstrip()
+    return genome
+
+def reverseComplement(s):
+    complement = {'A': 'T', 'C': 'G', 'G': 'C', 'T': 'A', 'N': 'N'}
+    t = ''
+    for base in s:
+        t = complement[base] + t
+    return t
+
+print(readGenome("lambda_virus.fa").count("AGGT") + readGenome("lambda_virus.fa").count(reverseComplement("AGGT")))
---
+++ Algorithms for DNA Sequencing 1/assignment_3.py
@@ -0,0 +1,29 @@
+# Question 3
+# What is the offset of the leftmost occurrence of \verb|ACTAAGT|ACTAAGT or
+# its reverse complement in the Lambda virus genome? E.g. if the leftmost occurrence of
+# \verb|ACTAAGT|ACTAAGT is at offset 40 (0-based) and the leftmost occurrence of its reverse complement
+# \verb|ACTTAGT|ACTTAGT is at offset 29, then report 29.
+
+def readGenome(filename):
+    genome = ''
+    with open(filename, 'r') as f:
+        for line in f:
+            if not line[0] == '>':
+                genome += line.rstrip()
+    return genome
+
+def reverseComplement(s):
+    complement = {'A': 'T', 'C': 'G', 'G': 'C', 'T': 'A', 'N': 'N'}
+    t = ''
+    for base in s:
+        t = complement[base] + t
+    return t
+
+a = readGenome("lambda_virus.fa").find("ACTAAGT")
+b = readGenome("lambda_virus.fa").find(reverseComplement("ACTAAGT"))
+
+if a >= b:
+    print(b)
+else:
+    print(a)
+
