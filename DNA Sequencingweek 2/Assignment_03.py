90f31450710aab0915736a83066d96e29f67ce18
NonaKuloshvili <nona.kuloshvili@gmail.com>
2020-08-25 00:55:52 +0400

    Week 2 homework added

---
+++ DNA Sequencingweek 2/Assignment_03.py
@@ -0,0 +1,46 @@
+# Question 3
+
+
+from bm_preproc import BoyerMoore
+
+def readGenome(filename):
+    genome = ''
+    with open(filename, 'r') as f:
+        for line in f:
+            # ignore header line with genome information
+            if not line[0] == '>':
+                genome += line.rstrip()
+    return genome
+
+def boyer_moore(p, p_bm, t):
+    """ Do Boyer-Moore matching. p=pattern, t=text,
+        p_bm=BoyerMoore object for p """
+    i = 0
+    occurrences = []
+    comparisons = 0
+    alignments = 0
+    while i < len(t) - len(p) + 1:
+        alignments += 1
+        shift = 1
+        mismatched = False
+        for j in range(len(p)-1, -1, -1):
+            comparisons += 1
+            if p[j] != t[i+j]:
+                skip_bc = p_bm.bad_character_rule(j, t[i+j])
+                skip_gs = p_bm.good_suffix_rule(j)
+                shift = max(shift, skip_bc, skip_gs)
+                mismatched = True
+                break
+        if not mismatched:
+            occurrences.append(i)
+            skip_gs = p_bm.match_skip()
+            shift = max(shift, skip_gs)
+        i += shift
+    return occurrences, comparisons, alignments
+
+
+p = "GGCGCGGTGGCTCACGCCTGTAATCCCAGCACTTTGGGAGGCCGAGG"
+chr1 = readGenome("chr1.GRCh38.excerpt.fasta")
+p_bm = BoyerMoore(p)
+
+print(boyer_moore(p, p_bm, chr1)[2])
