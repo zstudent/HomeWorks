e449c21fc224bd5e6d41a2dd76fb1c5cdfab798a
tinatin78 <tikunaghviniashvili@hotmail.com>
2020-08-26 01:02:59 +0400

    homework week 1 created

---
+++ Week  (1)/assignment_6.py
@@ -0,0 +1,38 @@
+#6
+def readGenome(filename):
+    genome = ''
+    with open(filename, 'r') as f:
+        for line in f:
+            if not line[0] == '>':
+                genome += line.rstrip()
+    return genome
+
+
+
+
+def reverseComplement(s):
+    complement = {'A': 'T', 'C': 'G', 'G': 'C', 'T': 'A', 'N': 'N'}
+    t = ''
+    for base in s:
+        t = complement[base] + t
+    return t
+
+
+def naive_2mm(p, t):
+    occurrences = []
+    for i in range(len(t) - len(p) + 1):
+        mismatches = 0
+        for j in range(len(p)):
+            if t[i + j] != p[j]:
+                mismatches += 1
+                if mismatches > 2:
+                    break
+        if mismatches <= 2:
+            occurrences.append(i)
+            break
+    return occurrences
+
+
+p = "AGGAGGTT"
+
+print(naive_2mm(p, readGenome("lambda_virus.fa")))
---
+++ Week  (1)/assignment_7.py
@@ -0,0 +1,31 @@
+#7
+def readFastq(filename):
+    sequences = []
+    qualities = []
+    with open(filename) as fh:
+        while True:
+            fh.readline()
+            seq = fh.readline().rstrip()
+            fh.readline()  # skip placeholder line
+            qual = fh.readline().rstrip()
+            if len(seq) == 0:
+                break
+            sequences.append(seq)
+            qualities.append(qual)
+    return sequences, qualities
+
+
+def lowest_quality_base(qs):
+    total = [0] * len(qs[0])
+    for q in qs:
+        for i, phred in enumerate(q):
+            total[i] += phred33ToQ(phred)
+    return total.index(min(total))
+
+
+def phred33ToQ(qual):
+    return ord(qual) - 33
+
+
+sequences, qualities = readFastq('ERR037900_1.first1000.fastq')
+print(lowest_quality_base(qualities))
