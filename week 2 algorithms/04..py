a0424f5a067e01a4bf2d332b9d91ef7fc4ead64a
tamarkobalia <qobaliatamari@gmail.com>
2020-08-26 00:52:35 +0400

    Added week 2 homework

---
+++ week 2 algorithms/04..py
@@ -0,0 +1,71 @@
+# 4
+import bisect
+
+def readGenome(filename):
+    genome = ''
+    with open(filename, 'r') as f:
+        for line in f:
+
+            if not line[0] == '>':
+                genome += line.rstrip()
+    return genome
+
+class Index(object):
+    def __init__(self, t, k):
+        ''' Create index from all substrings of size 'length' '''
+        self.k = k
+        self.index = []
+        for i in range(len(t) - k + 1):
+            self.index.append((t[i:i + k], i))
+        self.index.sort()
+
+    def query(self, p):
+        ''' Return index hits for first k-mer of P '''
+        kmer = p[:self.k]
+        i = bisect.bisect_left(self.index, (kmer, -1))
+        hits = []
+        while i < len(self.index):
+            if self.index[i][0] != kmer:
+                break
+            hits.append(self.index[i][1])
+            i += 1
+        return hits
+
+
+def approximate_match(p, t, n):
+    segment_length = int(round(len(p) / (n+1)))
+    all_matches = set()
+    p_idx = Index(t, segment_length)
+    idx_hits = 0
+    for i in range(n+1):
+        start = i*segment_length
+        end = min((i+1)*segment_length, len(p))
+        matches = p_idx.query(p[start:end])
+
+        for m in matches:
+            idx_hits += 1
+            if m < start or m-start+len(p) > len(t):
+                continue
+
+            mismatches = 0
+
+            for j in range(0, start):
+                if not p[j] == t[m-start+j]:
+                    mismatches += 1
+                    if mismatches > n:
+                        break
+            for j in range(end, len(p)):
+                if not p[j] == t[m-start+j]:
+                    mismatches += 1
+                    if mismatches > n:
+                        break
+
+            if mismatches <= n:
+                all_matches.add(m - start)
+    return list(all_matches), idx_hits
+
+
+p = "GGCGCGGTGGCTCACGCCTGTAAT"
+chr1 = readGenome('chr1.GRCh38.excerpt.fasta')
+
+print(len(approximate_match(p, chr1, 2)[0]))
