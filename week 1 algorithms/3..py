7d60c402172f305dc3a2368e83cc38978f7404d6
tamarkobalia <qobaliatamari@gmail.com>
2020-08-26 00:46:31 +0400

    Week 1 homework added

---
+++ week 1 algorithms/3..py
@@ -0,0 +1,24 @@
+3
+def readGenome(filename):
+    genome = ''
+    with open(filename, 'r') as f:
+        for line in f:
+            if not line[0] == '>':
+                genome += line.rstrip()
+    return genome
+
+def reverseComplement(s):
+    complement = {'A': 'T', 'C': 'G', 'G': 'C', 'T': 'A', 'N': 'N'}
+    t = ''
+    for base in s:
+        t = complement[base] + t
+    return t
+
+a = readGenome("lambda_virus.fa").find("ACTAAGT")
+b = readGenome("lambda_virus.fa").find(reverseComplement("ACTAAGT"))
+
+if a >= b:
+    print(b)
+else:
+    print(a)
+
