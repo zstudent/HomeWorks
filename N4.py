97f98f1416c2dfa7484f2024e584ec601c36b024
Tamar Gagnidze <tako.gagnidze@yahoo.com>
2020-09-02 20:28:48 +0400

    Homework for DNA week 4

--- N4.py
+++ N4.py
@@ -1,21 +1,20 @@
 from collections import defaultdict
-
-
 def readFastq(filename):
     sequences = []
     qualities = []
     with open(filename) as fh:
         while True:
-            fh.readline()
-            seq = fh.readline().rstrip()
-            fh.readline()
-            qual = fh.readline().rstrip()
+            fh.readline()  # skip name line
+            seq = fh.readline().rstrip()  # read base sequence
+            fh.readline()  # skip placeholder line
+            qual = fh.readline().rstrip()  # base quality line
             if len(seq) == 0:
                 break
             sequences.append(seq)
             qualities.append(qual)
     return sequences, qualities
 
+seqs, quals = readFastq('ads1_week4_reads.fq')
 
 def overlap(a, b, min_length=3):
     start = 0
@@ -25,18 +24,17 @@ def overlap(a, b, min_length=3):
             return 0
 
         if b.startswith(a[start:]):
-            return len(a) - start
+            return len(a)-start
         start += 1
 
-
 def overlap_graph(reads, k):
-
+    # Make index
     index = defaultdict(set)
     for read in reads:
         for i in range(len(read) - k + 1):
-            index[read[i:i + k]].add(read)
+            index[read[i:i+k]].add(read)
 
-
+    # Make graph
     graph = defaultdict(set)
     for r in reads:
         for o in index[r[-k:]]:
@@ -44,12 +42,36 @@ def overlap_graph(reads, k):
                 if overlap(r, o, k):
                     graph[r].add(o)
 
-    edges = 0
-    for read in graph:
-        edges += len(graph[read])
-    return (edges, len(graph))
+    return graph
+def pick_maximal_overlap(reads, k):
+    reada, readb = None, None
+    best_olen = 0
 
+    index = defaultdict(set)
+    for read in reads:
+        for i in range(len(read) - k + 1):
+            index[read[i:i+k]].add(read)
 
-seqs, quals = readFastq('ERR266411_1.for_asm.fastq')
-edges, suffixes = overlap_graph(seqs, 30)
-print(suffixes)
+    for r in reads:
+        for o in index[r[-k:]]:
+            if r != o:
+                olen = overlap(r, o, k)
+                if olen > best_olen:
+                    reada, readb = r, o
+                    best_olen = olen
+
+    return reada, readb, best_olen
+def greedy_scs(reads, k):
+    read_a, read_b, olen = pick_maximal_overlap(reads, k)
+    while olen > 0:
+        reads.remove(read_a)
+        reads.remove(read_b)
+        reads.append(read_a + read_b[olen:])
+        read_a, read_b, olen = pick_maximal_overlap(reads, k)
+    return ''.join(reads)
+
+for k in range(100, 1, -1):
+        genome = greedy_scs(seqs, k)
+        if len(genome) == 15894:
+            print(genome.count('T'))
+            break
